f21da278984e0528f61e87c2c6ce11af9af52875
LeticiaRutsatz <leticiarutsatz099@gmail.com>
2023-02-05 21:05:26 -0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -48,3 +48,73 @@ app.get("/growdevers", (request: Request, response: Response) => {
   );
   return response.status(200).json(growdevers);
 });
+
+app.get("/growdevers/:id", (request: Request, response: Response) => {
+  const  {id } = request.params
+  const growdever = listaGrowdevers.find((growdever) => growdever.id === id)
+  return response.status(200).json({Growdever: growdever?.paraModelJson()});
+});
+
+app.get("/growdevers", (request: Request, response: Response) => {
+  const { status, name} = request.query 
+
+  const growdevers = listaGrowdevers.filter((growdever) => {
+    if(status && name){
+      return growdever.nome === name && growdever.status === status
+    }
+
+    if(status || name){
+      return growdever.nome === name || growdever.status === status
+    }
+
+    return growdever
+  })
+
+  return response.status(200).json( growdevers);
+});
+
+
+app.post("/growdevers/:id/skills", (request: Request, response: Response) => {
+  const {id} = request.params;
+  const {skill} = request.body;
+
+  if(!skill){
+    return response.status(404).json({message: "Você precisa mandar uma nova skill"})
+  }
+
+  const existeGrowdever = listaGrowdevers.findIndex((growdever) => growdever.id === id)
+
+  if(existeGrowdever === -1) {
+    return response.status(400).json({message: "Este growdever não existe"});
+  }
+  
+  listaGrowdevers[existeGrowdever].habilidades.push(skill)
+
+
+  return response.status(201).json({habilidades : listaGrowdevers[existeGrowdever].habilidades});
+});
+
+app.delete("/growdevers/:id/skills/:name", (request: Request, response: Response) => {
+  const {id, name} = request.params;
+  const {skill} = request.body;
+
+  if(!skill){
+    return response.status(404).json({message: "Você precisa mandar uma nova skill"})
+  }
+
+  const existeGrowdever = listaGrowdevers.findIndex((growdever) => growdever.id === id)
+
+  if(existeGrowdever === -1) {
+    return response.status(400).json({message: "Este growdever não existe"});
+  }
+  
+  const existeSkill = listaGrowdevers[existeGrowdever].habilidades.findIndex((skill) => skill === name)
+
+  if(existeSkill === -1) {
+    return response.status(400).json({message: "Esta skill não existe"});
+  }
+
+  listaGrowdevers[existeSkill].habilidades.splice(existeSkill, 1)
+
+  return response.status(201).json({habilidades : listaGrowdevers[existeGrowdever].habilidades});
+});
